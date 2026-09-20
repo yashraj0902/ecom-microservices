a3495aa41cb5f58cb3acd7b1aff4c659fe0b7fd3
Yashraj <jadhavyash099@gmail.com>
2026-09-20 16:20:09 +0530

    Add POST for users

--- src/main/java/com/app/ecom/UserController.java
+++ src/main/java/com/app/ecom/UserController.java
@@ -1,9 +1,11 @@
 package com.app.ecom;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/users")
 public class UserController {
 
 
@@ -13,25 +15,34 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/api/users")
-    public List<User> getAllUsers()
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers()
     {
-        return userService.fetchAllUsers();
+        return new ResponseEntity<>(userService.fetchAllUsers(), HttpStatus.OK);
     }
 
-    @GetMapping("/api/users/{id}")
-    public User getUser(@PathVariable Long id)
-    {
-        return  userService.fetchUser(id);
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        return userService.fetchUser(id)
+                .map(user -> ResponseEntity.ok(user))
+                .orElseGet(() -> ResponseEntity.<User>notFound().build());
     }
 
-
-    @PostMapping("/api/users")
-    public String createUser(@RequestBody User user)
+    @PostMapping
+    public ResponseEntity<String> createUser(@RequestBody User user)
     {
        userService.addUser(user);
-       return "User added successfully";
+       return ResponseEntity.ok("User added successfully");
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateUser(@PathVariable Long id, @RequestBody User updatedUser)
+    {
+        boolean updated = userService.updateUser(id, updatedUser);
+        if(updated) return ResponseEntity.ok("User updated successfully!");
+        return ResponseEntity.notFound().build();
+    }
+
 
 }
 
--- src/main/java/com/app/ecom/UserService.java
+++ src/main/java/com/app/ecom/UserService.java
@@ -1,6 +1,7 @@
 package com.app.ecom;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 @Service
@@ -9,26 +10,29 @@ public class UserService {
     private List<User> userList = new ArrayList<>();
     private Long nextId = 1L;
 
-    public List<User> fetchAllUsers()
-    {
+    public List<User> fetchAllUsers() {
         return userList;
     }
 
-    public User fetchUser(Long id)
-    {
-        for (User user : userList)
-        {
-            if (user.getId().equals(id)) {
-                return user;
-            }
-        }
-
-        return null;
+    public Optional<User> fetchUser(Long id) {
+        return userList.stream()
+                .filter(user -> user.getId().equals(id))
+                .findFirst();
     }
 
     public List<User> addUser(User user) {
         user.setId(nextId++);
         userList.add(user);
         return userList;
+    }
+
+    public boolean updateUser(Long id, User updatedUser)
+    {
+        return userList.stream().filter(user -> user.getId().equals(id)).findFirst().map(existingUser -> {
+            existingUser.setFirstName(updatedUser.getFirstName());
+            existingUser.setLastName(updatedUser.getLastName());
+            return  true;
+        }).orElse(false);
+
     }
 }
